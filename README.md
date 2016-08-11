f923478bd160e1a410e93a45491aea46851c2a29
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-08-11 14:14:44 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# BLAST
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'BLAST'
+maintainer 'Eagle Genomics Ltd'
+maintainer_email 'chef@eaglegenomics.com'
+license 'apachev2'
+description 'Installs/Configures BLAST'
+long_description 'Installs/Configures BLAST'
+version '0.1.0'
