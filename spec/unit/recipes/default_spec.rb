0e6801093bbd3a05ce765bb293ef78d123d4d859
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-08-23 16:27:31 +0100

    lowercase name

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -18,7 +18,7 @@
 
 require 'spec_helper'
 
-describe 'BLAST::default' do
+describe 'blast::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
