eee917ea095765193254bb85ce927f64776a65b5
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-09-05 13:04:49 +0100

    rspec fix

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -19,12 +19,11 @@
 require 'spec_helper'
 
 describe 'blast::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+  context 'When all attributes are default, on ubuntu' do
+    # let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
 
+    # Make sure the converge works
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
