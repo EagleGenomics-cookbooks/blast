32825a7a00e21c14ec7476857f22a7b0b44e2c44
GreetDB <grdbaets@gmail.com>
2017-03-01 11:34:10 +0000

    add test for version

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -4,3 +4,7 @@ describe file('/usr/bin/blastn') do
   it { should be_file }
   it { should be_executable }
 end
+
+describe command('/usr/bin/blastn -version') do
+  its(:stdout) { should match(/2.6.0+/) }
+end
