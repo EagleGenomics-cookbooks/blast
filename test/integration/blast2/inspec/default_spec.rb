b183fe96ce8c7ddd31011fb8a2abb3f016511ded
GreetDB <grdbaets@gmail.com>
2017-03-01 13:20:37 +0000

    add new suite to test versions

---
+++ test/integration/blast2/inspec/default_spec.rb
@@ -0,0 +1,10 @@
+
+# Check that the installation was successfull
+describe file('/usr/bin/blastn') do
+  it { should be_file }
+  it { should be_executable }
+end
+
+describe command('/usr/bin/blastn -version') do
+  its(:stdout) { should match(/2.4.0+/) }
+end
